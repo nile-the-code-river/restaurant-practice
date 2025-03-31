dfb6840c427d9fecc9fb8b262e515a5bee6b228e
Nile Kim <nile.coderiver@gmail.com>
2025-03-31 12:31:01 +0900

    add model classes to domain layer

---
+++ RestaurantsPractice.Domain/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Address
+{
+    public required string City { get; set; }
+    public required string Street { get; set; }
+    public required string PostalCode { get; set; }
+}
--- RestaurantsPractice.Domain/Entities/Restaurant.cs
+++ RestaurantsPractice.Domain/Entities/Restaurant.cs
@@ -8,5 +8,17 @@ namespace RestaurantsPractice.Domain.Entities
 {
     public class Restaurant
     {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Category { get; set; }
+        public required bool HasDelivery { get; set; }
+
+        public string? ContactEmail { get; set; }
+        public string? ContactNumber { get; set; }
+
+        public Address? Address { get; set; }
+
+        public required List<Dish> Dishes { get; set; }
     }
 }
